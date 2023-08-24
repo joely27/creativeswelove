4c8d6f4a0cdeb3d1e7cd6590a2a225b06ac7ba48
joely27 <139440738+joely27@users.noreply.github.com>
2023-08-24 09:55:21 +0100

    yooooo

---
+++ 1692847881522x900873944556634100.js
@@ -0,0 +1,29 @@
+// Create a <style> element
+var styleElement = document.createElement('style');
+document.head.appendChild(styleElement);
+
+// Define your CSS rules
+var css = `
+iframe {
+width: 100%;
+height: 672px;
+border: 0px!important;
+
+}
+
+body {
+
+margin: 0;
+}
+
+@media (max-width: 900px) {
+iframe {
+height: 1204px;
+
+}
+
+}
+`;
+
+// Add the CSS rules to the <style> element
+styleElement.appendChild(document.createTextNode(css));
